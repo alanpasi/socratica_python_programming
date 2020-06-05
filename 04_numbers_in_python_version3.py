7c35d73b9ebd4e69268fabc35617d2e7da4cef7d
alanpasi <alanpasi@gmail.com>
2020-06-05 18:16:04 -0300

    Added lesson 04

---
+++ 04_numbers_in_python_version3.py
@@ -0,0 +1,15 @@
+# Types os numbers: int, long, float, complex
+
+a = 496  # This is a perfect number
+print(type(a))
+print(a)
+
+e = 2.718182818
+print(e)
+print(type(e))
+
+z = 2 + 6.1j
+print(z)
+print(type(z))
+print(z.real)
+print(z.imag)
